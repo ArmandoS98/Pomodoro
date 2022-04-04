0eecc9f112ecc05eb382c3fb41a1b1d43e5779f3
ArmandoS98 <armandosantosec@gmail.com>
2022-04-04 17:00:42 -0600

    implementacion del CountDownTimer

---
+++ app/src/main/java/com/techun/pomodoro/ui/ConfigPomodoroFragment.kt
@@ -0,0 +1,19 @@
+package com.techun.pomodoro.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.techun.pomodoro.R
+
+class ConfigPomodoroFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_config_pomodoro, container, false)
+    }
+}
