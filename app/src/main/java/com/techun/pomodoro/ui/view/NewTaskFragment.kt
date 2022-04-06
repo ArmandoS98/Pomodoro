0ed2de0020e5439dda0bdc7de79bfd430abca095
ArmandoS98 <armandosantosec@gmail.com>
2022-04-05 20:03:19 -0600

    mejora de la UI

--- app/src/main/java/com/techun/pomodoro/ui/view/NewTaskFragment.kt
+++ app/src/main/java/com/techun/pomodoro/ui/view/NewTaskFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
-import com.google.android.material.chip.Chip
-import com.techun.pomodoro.databinding.ChipBinding
+import com.techun.pomodoro.R
 import com.techun.pomodoro.databinding.FragmentNewTaskBinding
 
 
@@ -24,28 +25,40 @@ class NewTaskFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupChip()
+        //Tasks
+        val tasks = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, tasks)
+        (binding.tilTask.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+        //Work Interval
+        val workInterval = listOf(15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30)
+        val adapterWorkInterval = ArrayAdapter(requireContext(), R.layout.list_item, workInterval)
+        (binding.tilWorkInterval.editText as? AutoCompleteTextView)?.setAdapter(adapterWorkInterval)
+
+        //Short Break
+        val adapterShortBreak = ArrayAdapter(requireContext(), R.layout.list_item, tasks)
+        (binding.tilShortBreak.editText as? AutoCompleteTextView)?.setAdapter(adapterShortBreak)
     }
 
-    private fun setupChip() {
-        for (i in 1..30) {
-            val chip = createChip(i.toString())
-            binding.cgTask.addView(chip)
-        }
-        for (i in 1..30) {
-            val chip = createChip(i.toString())
-            binding.cgWorkInterval.addView(chip)
-        }
-        for (i in 1..30) {
-            val chip = createChip(i.toString())
-            binding.cgShortBreak.addView(chip)
-        }
-    }
+    /*  private fun setupChip() {
+          for (i in 1..10) {
+              val chip = createChip(i.toString())
+              binding.cgTask.addView(chip)
+          }
+          for (i in 15..30) {
+              val chip = createChip(i.toString())
+              binding.cgWorkInterval.addView(chip)
+          }
+          for (i in 1..10) {
+              val chip = createChip(i.toString())
+              binding.cgShortBreak.addView(chip)
+          }
+      }
 
-    private fun createChip(label: String): Chip {
-        val chip = ChipBinding.inflate(layoutInflater).root
-        chip.text = label
-        return chip
-    }
+      private fun createChip(label: String): Chip {
+          val chip = ChipBinding.inflate(layoutInflater).root
+          chip.text = label
+          return chip
+      }*/
 
 }
